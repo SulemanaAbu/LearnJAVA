a597d16f27f239f04fb2ef36ffc1d03c5029a0d7
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 13:51:11 +0000

    Simple GUI ownTry

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
-import javax.swing.JOptionPane;
-public class Main {
+ import javax.swing.JOptionPane;
 
-    public static void main(String [] args){
-       // GUI interface in Java
+public class Main{
+    public static void main (String [] args){
 
         String name = JOptionPane.showInputDialog("Enter your name");
-        JOptionPane.showMessageDialog(null , "Hello "+name);
+        JOptionPane.showMessageDialog(null, "Your name is "+name);
 
-        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
-        JOptionPane.showMessageDialog(null , "You are  "+age+" years old");
+        String school = JOptionPane.showInputDialog("Enter the name of your school");
+        JOptionPane.showMessageDialog(null, "You attend "+school);
 
-        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
-        JOptionPane.showMessageDialog(null , "You are  "+height+" feets tall");
+        String program = JOptionPane.showInputDialog("Enter the program you offer");
+        JOptionPane.showMessageDialog(null, "You offer "+program);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog(null, "How old are you?"));
+        JOptionPane.showMessageDialog(null, "You are "+age+" years old");
+
+        double gpa = Double.parseDouble(JOptionPane.showInputDialog("Enter your current gpa"));
+        JOptionPane.showMessageDialog(null, "Your gpa is "+gpa);
+
 
 
 
