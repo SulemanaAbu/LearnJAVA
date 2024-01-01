c8f131585c8eeddc841dfd1681fa19479fb4d210
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 16:43:13 +0000

    Logical operators

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,23 @@
 import java.util.Scanner;
+
 public class Main{
 
     public static void main(String [] args) {
-        //Switch case
+        //AND OR NOT logical operators
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the day you were born: ");
-        String day = scanner.nextLine();
 
-        switch(day){
-            case "Sunday" : System.out.println("You were born on Sunday");
-            break;
-            case "Monday" : System.out.println("You were born on Monday");
-                break;
-            case "Tuesday" : System.out.println("You were born on Tuesday");
-                break;
-            case "Wednesday" : System.out.println("You were born on Wednesday");
-                break;
-            case "Thursday" : System.out.println("You were born on Thursday");
-                break;
-            case "Friday" : System.out.println("You were born on Friday");
-                break;
-            case "Saturday" : System.out.println("You were born on Saturday");
-                break;
-            default : System.out.println("It is not a day");
+        System.out.println("You are playing a game! Press q or Q to quit");
+        String response = scanner.nextLine();
+
+        if (response.equals("q") || response.equals("Q") ){
+            System.out.println("You have quit the game ");
+        }
+        else{
+            System.out.println("Enter q or Q to quit the game");
         }
 
+
+
+    scanner.close();
     }
 }
