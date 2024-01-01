755d5319084be782fccc0b4970e87a105c144e74
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 16:47:23 +0000

    Logical operators2

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class Main{
 
     public static void main(String [] args) {
-        //AND, OR, NOT logical operators
+        //AND(&&), OR(||), NOT(!) logical operators
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("You are playing a game! Press q or Q to quit");
         String response = scanner.nextLine();
-
+//When ! is added before the argument in
+// if bracket
+        // it negates the statement
         if (response.equals("q") || response.equals("Q") ){
             System.out.println("You have quit the game ");
         }
         else{
-            System.out.println("Enter q or Q to quit the game");
+            System.out.println("You are still playing the game");
         }
 
 
