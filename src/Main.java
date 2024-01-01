6578ad11314f9f372d80941916cf0d2222a1299a
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 11:29:42 +0000

    Variable printed

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,11 @@ public class Main {
         double y = 3.142;
         String name = "SulemanaAbu";
         char letter = 'A';
+        boolean cash = true;
+
+        System.out.println("The quantity is: "+x);
+        System.out.println("Buyer's name is: "+name);
+        System.out.println("Identification letter: "+letter);
+        System.out.println("Payed with cash: "+cash);
     }
 }
