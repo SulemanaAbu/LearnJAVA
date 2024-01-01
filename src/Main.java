496970c6b61a106d4cee9feac2c087fd35025fe5
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 14:37:05 +0000

    Math Java ownTry

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,30 @@ public class Main{
 
     public static void main(String [] args){
         // JAVA Math Class
-        // Finding the hypotenuse of a triangle
-
-        double x;
-        double y;
-        double z;
+        // Almighty Formula
+        int a;
+        int b;
+        int c;
+        double pos;
+        double neg;
+        double x1;
+        double x2;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the value of x: ");
-        x = scanner.nextDouble();
+        System.out.println("Enter a: ");
+        a = scanner.nextInt();
+        System.out.println("Enter b: ");
+        b = scanner.nextInt();
+        System.out.println("Enter c: ");
+        c = scanner.nextInt();
 
-        System.out.println("Enter the value of y: ");
-        y = scanner.nextDouble();
+        pos = -b+Math.sqrt((b*b) - (4*a*c));
+        x1 = pos / (2*a);
+        neg = -b-Math.sqrt((b*b) - (4*a*c));
+        x2 = neg / (2*a);
 
-        z = Math.sqrt((x*x) + (y*y)) ;
-        System.out.println("The value of the hypotenuse is: "+z);
+        System.out.println("The first value is: "+x1);
+        System.out.println("The second value is: "+x2);
 
 scanner.close();
     }
