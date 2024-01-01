bc8efd1089e020e5cfbfbd8db985556c0f305a12
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 22:08:02 +0000

    for-each loop

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,21 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public class Main{
 
     public static void main(String [] args) {
-      // 2D ArrayLists
-        ArrayList<ArrayList<String>> groceryList = new ArrayList();
+      // ForEach loop
+       //String [] animals = {"dog", "cat", "sheep", "goat"};
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add("dog");
+        animals.add("cat");
+        animals.add("sheep");
+        animals.add("goat");
 
-        ArrayList<String> bakeryList = new ArrayList();
-        bakeryList.add("pasta");
-        bakeryList.add("garlic bread");
-        bakeryList.add("donuts");
+       for(String i : animals){
+           System.out.println(i);
 
-        ArrayList<String> produceList = new ArrayList();
-        produceList.add("tomatoes");
-        produceList.add("zucchini");
-        produceList.add("carrots");
-        produceList.add("peppers");
+       }
 
-        ArrayList<String> drinkList = new ArrayList();
-        drinkList.add("soda");
-        drinkList.add("coke");
-
-        groceryList.add(bakeryList);
-        groceryList.add(produceList);
-        groceryList.add(drinkList);
-
-        System.out.println(groceryList.get(0).get(0));
 
     }
 }
