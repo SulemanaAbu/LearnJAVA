dd4363ac75042b9fe32be7cf24bc4e2b69b892d5
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 21:03:45 +0000

    2Darrays

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,28 @@
 public class Main{
 
     public static void main(String [] args) {
-      //Arrays
-     //   String [] cars = {"Camaro", "Toyota", "BMW", "Ferrari"};
-        String [] cars = new String[4];
-        cars[0] = "Camaro";
-        cars[1] = "Toyota";
-        cars[2] = "BMW";
-        cars[3] = "Ferrari";
+      // 2D Arrays
+        String [][] cars = new String[3][3];
 
-        // use loops to display all elements in the array
-        for (String car : cars) {
-            System.out.println(car);
+        cars [0][0] = "Camaro";
+        cars [0][1] = "Corvette";
+        cars [0][2] = "Tesla";
+
+        cars [1][0] = "Sonata";
+        cars [1][1] = "Ferrari";
+        cars [1][2] = "Civic";
+
+        cars [2][0] = "Range Rover";
+        cars [2][1] = "Mustang";
+        cars [2][2] = "Lambo";
+
+        for (int i=0; i< cars.length;i++) {
+            System.out.println();
+            for (int j=0; j<cars[i].length; j++) {
+                System.out.print(cars[i][j]+" ");
+
+            }
         }
+
     }
 }
