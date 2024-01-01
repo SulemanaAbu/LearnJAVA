fd90229fa9ddaacde3ff8ce9defe455b9a283db8
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 14:49:19 +0000

    Random_numGenerartion

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,16 @@
-import java.util.Scanner;
+import java.util.Random;
 public class Main{
 
     public static void main(String [] args){
-        // JAVA Math Class
-        // Almighty Formula
-        int a;
-        int b;
-        int c;
-        double pos;
-        double neg;
-        double x1;
-        double x2;
-        Scanner scanner = new Scanner(System.in);
+        //Random Numbers
+        Random random = new Random();
 
-        System.out.println("Enter a: ");
-        a = scanner.nextInt();
-        System.out.println("Enter b: ");
-        b = scanner.nextInt();
-        System.out.println("Enter c: ");
-        c = scanner.nextInt();
+        int x = random.nextInt(6)+1;
+        System.out.println(x);
+        double y = random.nextDouble();
+        System.out.println(y);
+        boolean z = random.nextBoolean();
+        System.out.println(z);
 
-        pos = -b+Math.sqrt((b*b) - (4*a*c));
-        x1 = pos / (2*a);
-        neg = -b-Math.sqrt((b*b) - (4*a*c));
-        x2 = neg / (2*a);
-
-        System.out.println("The first value is: "+x1);
-        System.out.println("The second value is: "+x2);
-
-scanner.close();
     }
 }
