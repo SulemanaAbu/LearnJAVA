05504fef4781f69022b5fb548cce95df60204111
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 17:17:50 +0000

    While loop

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,15 @@
 import java.util.Scanner;
-
 public class Main{
 
     public static void main(String [] args) {
-        //AND(&&), OR(||), NOT(!) logical operators
+       // while loops
         Scanner scanner = new Scanner(System.in);
+        String name = "";
 
-        System.out.println("You are playing a game! Press q or Q to quit");
-        String response = scanner.nextLine();
-/*When ! is added before the argument in
-if bracket
-        // it negates the statement*/
-        if (response.equals("q") || response.equals("Q") ){
-            System.out.println("You have quit the game ");
+        while(name.isBlank()){
+            System.out.print("Enter your name: ");
+             name = scanner.nextLine();
         }
-        else{
-            System.out.println("You are still playing the game");
-        }
-
-
-
-    scanner.close();
+        System.out.println("Hello "+name);
     }
 }
