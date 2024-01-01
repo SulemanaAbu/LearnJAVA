2b8319105ef75bbd705d9da3972ba8088c45439f
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 16:11:25 +0000

    Switch_Cases

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,26 @@
-import java.util.Scanner;
+
 public class Main{
 
-    public static void main(String [] args){
-        //If and else statements
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String [] args) {
+        //Switch cases
+        String day = "Bread";
+        switch(day){
+            case "Sunday" : System.out.println("It is Sunday");
+            break;
+            case "Monday" : System.out.println("It is Monday");
+                break;
+            case "Tuesday" : System.out.println("It is Tuesday");
+                break;
+            case "Wednesday" : System.out.println("It is Wednesday");
+                break;
+            case "Thursday" : System.out.println("It is Thursday");
+                break;
+            case "Friday" : System.out.println("It is Friday");
+                break;
+            case "Saturday" : System.out.println("It is Saturday");
+                break;
+            default : System.out.println("It is not a day");
+        }
 
-        System.out.println("Enter your score: ");
-        int score = scanner.nextInt();
-        System.out.println("Your score is "+score);
-
-        // Grade determination
-        if (score > 100) {
-            System.out.println("Error checking grade!");
-        }
-        else if (score < 0) {
-            System.out.println("Error checking grade!");
-        }
-        else if (score >= 80) {
-            System.out.println("Your grade is A");
-        }
-        else if (score >= 70){
-            System.out.println("Your grade is B");
-        }
-        else if (score >= 60){
-            System.out.println("Your grade is C");
-        }
-        else if (score >= 50){
-            System.out.println("Your grade is D");
-        }
-        else if (score >= 40){
-            System.out.println("Your grade is E");
-        }
-        else{
-            System.out.println("Your grade is F");
-        }
     }
 }
