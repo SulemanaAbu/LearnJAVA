1469fc08a1d69f9d4a91e87498118cd671be199e
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 21:33:36 +0000

    ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,20 @@
+import java.util.ArrayList;
 
 public class Main{
 
     public static void main(String [] args) {
-      // 2D Arrays
-        String [][] cars = new String[3][3];
+      // arraylists
+        ArrayList<String> food = new ArrayList<String>();
 
-        cars [0][0] = "Camaro";
-        cars [0][1] = "Corvette";
-        cars [0][2] = "Tesla";
+        food.add("Pizza");
+        food.add("Hamburger");
+        food.add("Hot dog");
 
-        cars [1][0] = "Sonata";
-        cars [1][1] = "Ferrari";
-        cars [1][2] = "Civic";
+        for(int i=0; i<food.size(); i++ ){
+            System.out.println(food.get(i));
 
-        cars [2][0] = "Range Rover";
-        cars [2][1] = "Mustang";
-        cars [2][2] = "Lambo";
-
-        for (String[] car : cars) {
-            System.out.println();
-            for (String s : car) {
-                System.out.print(s + " ");
-
-            }
         }
+
 
     }
 }
