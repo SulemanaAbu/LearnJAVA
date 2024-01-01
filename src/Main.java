bbdcb01eb8c4ecbc8f62621932d5684b3979cf74
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 15:17:49 +0000

    If and else statements

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,39 @@
-import java.util.Random;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 public class Main{
 
     public static void main(String [] args){
-        //Random Numbers
-        Random random = new Random();
+        //If and else statements
+        Scanner scanner = new Scanner(System.in);
 
-        int x = random.nextInt(6)+1;
-        System.out.println(x);
-        double y = random.nextDouble();
-        System.out.println(y);
-        boolean z = random.nextBoolean();
-        System.out.println(z);
+        System.out.println("Enter your score: ");
+        int score = scanner.nextInt();
+        System.out.println("Your score is "+score);
 
+        // Grade determination
+        if (score > 100) {
+            System.out.println("Error checking grade!");
+        }
+        else if (score < 0) {
+            System.out.println("Error checking grade!");
+        }
+        else if (score >= 80) {
+            System.out.println("Your grade is A");
+        }
+        else if (score >= 70){
+            System.out.println("Your grade is B");
+        }
+        else if (score >= 60){
+            System.out.println("Your grade is C");
+        }
+        else if (score >= 50){
+            System.out.println("Your grade is D");
+        }
+        else if (score >= 40){
+            System.out.println("Your grade is E");
+        }
+        else{
+            System.out.println("Your grade is F");
+        }
     }
 }
