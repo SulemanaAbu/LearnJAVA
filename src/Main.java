697c6df85d499b885a50ad7d01e760cf9fa4a97e
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 17:41:28 +0000

    nestedFor loop

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
-
+import java.util.Scanner;
 public class Main{
 
     public static void main(String [] args) {
-       // for loop
-        for (int i=10; i>=0; i-=5){
-            System.out.println(i);
+       // nested loops
+        Scanner scanner = new Scanner(System.in);
+        int rows;
+        int columns;
+        String symbol;
+
+        System.out.println("Enter # of rows: ");
+        rows = scanner.nextInt();
+
+        System.out.println("Enter # of columns: ");
+        columns = scanner.nextInt();
+
+        System.out.println("Enter symbol to use: ");
+        symbol = scanner.next();
+
+        for (int i=1; i<=rows; i++){
+            System.out.println();
+            for(int j=1; j<=columns; j++){
+                System.out.print(symbol);
+            }
         }
-        System.out.println("Happy new Year!");
     }
 }
