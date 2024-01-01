919860d74697283a87cf308d1871b6cfe6182a7e
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-01 21:04:26 +0000

    2Darrays

--- src/Main.java
+++ src/Main.java
@@ -17,10 +17,10 @@ public class Main{
         cars [2][1] = "Mustang";
         cars [2][2] = "Lambo";
 
-        for (int i=0; i< cars.length;i++) {
+        for (String[] car : cars) {
             System.out.println();
-            for (int j=0; j<cars[i].length; j++) {
-                System.out.print(cars[i][j]+" ");
+            for (String s : car) {
+                System.out.print(s + " ");
 
             }
         }
