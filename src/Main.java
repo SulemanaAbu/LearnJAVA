ef5bc1571bfcd10a3af54c4b1cffb1ec2d2fd9b0
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-02 17:42:31 +0000

    Exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,27 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main{
 
     public static void main(String [] args) {
-        //Dynamic Polymorphism
-
-        Scanner scanner  = new Scanner(System.in);
-        Animal  animal   = new Animal();
-        Animal  animal1  = new Dog();
-        Animal  animal2  = new Cat();
-
-        System.out.println("What animal do you want? ");
-        System.out.println("(1=dog) or (2=cat): ");
-        int choice = scanner.nextInt();
-
-        if (choice==1){
-           animal1.speak();
+        //Java exception handling
+        try (Scanner scanner = new Scanner(System.in)) {
+            try {
+                System.out.println("Enter a whole number to divide: ");
+                int x = scanner.nextInt();
+                System.out.println("Enter a whole number to divide: ");
+                int y = scanner.nextInt();
+                int z = x / y;
+                System.out.println("result: " + z);
+            } catch (ArithmeticException e) {
+                    System.out.println("You can't divide by zero! IDIOT");
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a number OMG !!!");
+            } catch (Exception e) {
+                System.out.println("Oops! Something went wrong");
+            } finally {
+                scanner.close();
+            }
         }
-        else if(choice==2){
 
-            animal2.speak();
-        }
-        else{
-            System.out.println("That choice was invalid");
-            animal.speak();
-        }
     }
 }
