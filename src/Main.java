6cdb1881e7c856515c955cb2d171a186c0b40caa
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-02 19:46:22 +0000

    file class

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,19 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.io.File;
+
 public class Main{
 
     public static void main(String [] args) {
-        //Java exception handling
-        try (Scanner scanner = new Scanner(System.in)) {
-            try {
-                System.out.println("Enter a whole number to divide: ");
-                int x = scanner.nextInt();
-                System.out.println("Enter a whole number to divide: ");
-                int y = scanner.nextInt();
-                int z = x / y;
-                System.out.println("result: " + z);
-            } catch (ArithmeticException e) {
-                    System.out.println("You can't divide by zero! IDIOT");
-            } catch (InputMismatchException e) {
-                System.out.println("Please enter a number OMG !!!");
-            } catch (Exception e) {
-                System.out.println("Oops! Something went wrong");
-            } finally {
-                scanner.close();
-            }
+        //Java file class
+        File file = new File("C:/Users/saddi/OneDrive/Desktop/secret_message.txt/");
+
+        if (file.exists()){
+            System.out.println("That file exists");
+            System.out.println(file.getAbsolutePath());
+            System.out.println(file.isFile());
+
+        }
+        else{
+            System.out.println("That file does not exist");
         }
 
     }
