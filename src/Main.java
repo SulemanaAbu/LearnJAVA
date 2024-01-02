d852f07d0eaa1403b8d75eac4b58d502542f0ecb
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-02 09:19:23 +0000

    Java variable scope un

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,12 @@ public class Main{
 
     public static void main(String [] args) {
     //Java variable Scope
-        DiceRoller diceRoller = new DiceRoller();
+       DiceRoller diceRoller = new DiceRoller();
+        // Random random;
+       // int number;
+       //  random = new Random();
+       //  number = random.nextInt(6)+1;
+       // System.out.println(number);*
 
     }
 }
