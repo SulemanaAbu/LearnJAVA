6034ef8ad44a6acf7e0c8ff3b67c1a78e0a5b71b
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-02 16:47:36 +0000

    Java Interface

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,14 @@
 public class Main{
 
     public static void main(String [] args) {
-        //Java copy objects
-        Car car1 = new Car("Chevrolet","Camaro",2020);
-        Car car2 = new Car("BMW", "Mustang",2021);
+        //Java Interface
+        Fish fish = new Fish();
+        Rabbit rabbit = new Rabbit();
+        Hawk hawk = new Hawk();
 
-        car1.copy(car2);
-
-        System.out.println(car1);
-        System.out.println(car2);
-        System.out.println();
-        System.out.println(car1.getMake());
-        System.out.println(car1.getModel());
-        System.out.println(car1.getYear());
-        System.out.println();
-        System.out.println(car2.getMake());
-        System.out.println(car2.getModel());
-        System.out.println(car2.getYear());
+    rabbit.flee();
+    hawk.hunt();
+    fish.flee();
+    fish.hunt();
     }
 }
