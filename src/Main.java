4b7d3c5a8f18462e0d2273c86e8801b8bf75f6a3
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-02 06:26:58 +0000

    Java variable scope

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,8 @@
 public class Main{
 
     public static void main(String [] args) {
-     //OBJECT ORIENTED PROGRAMMING
-        //CONSTRUCTORS
-        Human human1 = new Human("Bro", 18, 60);
-        Human human2 = new Human("Cherry", 21, 45);
-
-        human1.drink();
-        human2.eat();
+    //Java variable Scope
+        DiceRoller diceRoller = new DiceRoller();
 
     }
 }
