e9006fba52d515fb38602da27f51edf097450a3c
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-02 14:12:26 +0000

    super keyword

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ public class Main{
 
     public static void main(String [] args) {
 
-        //Overriding
-       Father father = new Father ();
-       Son son = new Son();
+        //Super keyword
+        Hero hero1 = new Hero("Batman",42,"Everything");
+        Hero hero2 = new Hero("Suleman",20,"code");
+        System.out.println(hero1.toString());
+        System.out.println();
+        System.out.println(hero2.toString());
 
-       father.him();
-       son.him();
+
     }
 }
