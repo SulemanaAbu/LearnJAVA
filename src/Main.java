452653938ac6a760e3b88c20a6b144e218ef3a16
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-02 15:27:35 +0000

    Encapsulation

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 
 public class Main{
-//Main class
-    public static void main(String [] args) {
 
-        //Abstraction
-       Son son = new Son();
-       son.him();
+    public static void main(String [] args) {
+        //Encapsulation
+        Car car = new Car("Chevrolet","Camaro", 2020);
+        //Setters
+        car.setYear(2021);
+        car.setMake("Tesla");
+        car.setModel("Doge");
+        //Getters
+        System.out.println(car.getMake());
+        System.out.println(car.getModel());
+        System.out.println(car.getYear());
+
+
 
     }
 }
