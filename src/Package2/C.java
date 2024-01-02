3ee06ad1a5dfb2a8735c9002cb216001062a4dd7
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-02 15:01:29 +0000

    Modifiers

---
+++ src/Package2/C.java
@@ -0,0 +1,7 @@
+package Package2;
+//import Package1.*;
+public class C {
+    public String publicMessage = "This is public message";
+    private String privateMessage = "This is private";
+    String defaultMessage = "This is the default message";
+}
