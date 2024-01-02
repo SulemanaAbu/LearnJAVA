44695699581a05d771671feef3d022ae80df832b
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-02 05:22:12 +0000

    Final keyword

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,9 @@
 public class Main{
 
     public static void main(String [] args) {
-     // printf method
-        //%d for positioning decimals
-        /*System.out.printf("%d The jersey number of Mohammed Kuku", 14);*/
-       // boolean myBoolean = true;
-       // char myChar = '@';
-       // String myString = "Tate";
-       // int myInt = 50;
-        double myDouble = 1000.4503;
+     //final keyword
+        final double PI= 3.142;
 
-        // System.out.printf("%b", myBoolean);
-       // System.out.printf("%c", myChar);
-       // System.out.printf("%d", myInt);
-        // System.out.printf("Hello %s", myString);
-       // System.out.printf("%f", myDouble);
-
-        //[width] System.out.printf("Hello %5s", myString);
-        System.out.printf("%.2f", myDouble);
+        System.out.println(PI);
     }
 }
