8c4485b082fa9b2b66718c1c5ab6ef32ab82fe14
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-02 21:10:29 +0000

    file reader

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
-
-import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Main{
 
     public static void main(String [] args) {
-        //Java file writer
+        //Java file reader
         try {
-            FileWriter writer = new FileWriter("C:/Users/saddi/IdeaProjects/MyFirstProgram/poem.txt");
-            writer.write("Roses are red \nViolets are Blue \nBooty booty booty \nRock and roll");
-            writer.append("\n(A poem by Bro)");
-            writer.close();
+            FileReader reader = new FileReader("poem.txt");
+            int data = reader.read();
+            while(data!=-1){
+                System.out.println((char)data);
+                data = reader.read();
+            }
+            reader.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
 }
